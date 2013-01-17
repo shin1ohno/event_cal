03c89b4c64f858b40851b7bd91b8480a2995cd45
Ohno Shin'ichi <shin1ohno@me.com>
2013-01-17 15:09:08 +0900

    Move files

--- spec/models/event_cal/event_spec.rb
+++ spec/models/event_cal/event_spec.rb
@@ -1,5 +1,5 @@
-require_relative '../spec_helper'
-require_relative '../../lib/models/event'
+require_relative '../../spec_helper'
+require_relative '../../../app/models/event_cal/event'
 
 require 'active_support/core_ext'
 
